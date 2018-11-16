b6b987048356b3fe1a51d2d6528d44474a5a94a7
Daniel Felipe Manso Rojas <1112883111@academico.icesi.edu.co>
2018-11-16 09:01:57 -0500

    2 commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 const fs = require('fs');
-const download = require('download');
+
 
 const app = express();
 
@@ -11,26 +11,7 @@ app.engine('handlebars', hbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', function(request, response){
-    response.render('home');
-});
-
-app.get('/api/descargar', function(request, response){
-    //EL NOMBRE DE LA VARIABLE DEBE SER IGUAL TANTO 
-    //          ACÁ COMO EN EL INPUT DEL HTML
-    var image__name = request.query.image__name;
-    var image__url = request.query.image__url;
-
-    if(image__name !== null && image__url !== null){
-        download(image__url.valueOf()).then(data =>{
-            fs.writeFileSync('imagenes/'+image__name+'.jpg', data);
-        });
-    
-       // response.send(image__name+' Descargado');
-        response.render('home');
-    } else {
-        response.send('MUCHO BOBO HIJUEPUTA');
-    }
-
+    response.render('paginaA');
 });
 
 app.listen(5500);
